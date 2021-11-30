c222326d202fcf3df28f955658d2eb4a7fda88e2
EnochQin <qyqdream@gmail.com>
2021-11-30 11:51:35 +0800

    add: 66 plusOne

---
+++ src/66.加一.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=javascript
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  const length = digits.length;
+  for (let i = length - 1; i >= 0; i--) {
+    if (digits[i] !== 9) {
+      digits[i]++;
+      return digits;
+    } else {
+      digits[i] = 0;
+    }
+  }
+
+  const result = new Array(length + 1).fill(0);
+  result[0] = 1;
+  return result;
+};
+// @lc code=end
